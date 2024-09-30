03aee9a862298f8d7955ffdd073e8ddfa20c219e
Filipe Vieira <filipe.vieira@scott-sports.com>
2024-09-30 15:47:50 +0200

    working on login-form 1.11.26

--- src/components/auth/login-form.tsx
+++ src/components/auth/login-form.tsx
@@ -1,7 +1,28 @@
+"use client";
+
+import * as z from "zod";
 import React from "react";
 import CardWrapper from "./card-wrapper";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { LoginSchema } from "@/schemas";
 
 export default function LoginForm() {
+  const form = useForm<z.infer<typeof LoginSchema>>({
+    resolver: zodResolver(LoginSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
   return (
     <CardWrapper
       headerLabel="Welcome back"
@@ -9,7 +30,11 @@ export default function LoginForm() {
       backButtonHref="/auth/register"
       showSocial
     >
-      Login Form
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(() => {})} className="space-y-6">
+          <div className="space-y-4"></div>
+        </form>
+      </Form>
     </CardWrapper>
   );
 }
