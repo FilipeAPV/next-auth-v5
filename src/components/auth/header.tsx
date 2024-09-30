28bb9731977394e37c4afa297d337d6a6b5ac623
Filipe Vieira <filipe.vieira@scott-sports.com>
2024-09-30 14:36:16 +0200

    card wrapper set up

---
+++ src/components/auth/header.tsx
@@ -0,0 +1,14 @@
+type HeaderProps = {
+  label: string;
+};
+
+import React from "react";
+
+export default function Header({ label }: HeaderProps) {
+  return (
+    <div className="w-full flex flex-col gap-y-4 items-center justify-center">
+      <h1 className="text-3xl font-semibold ">Auth</h1>
+      <p className="text-muted-foreground text-sm">{label}</p>
+    </div>
+  );
+}
