2d2a8b488072cf55d2788e4e9180956f22328d48
Filipe Vieira <filipe.vieira@scott-sports.com>
2024-10-01 15:07:19 +0200

    callbacks 32037

--- src/auth.ts
+++ src/auth.ts
@@ -1,9 +1,52 @@
-import NextAuth from "next-auth";
+import NextAuth, { type DefaultSession } from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import authConfig from "./auth.config";
 import { db } from "./lib/db";
+import { getUserById } from "./data/user";
+import { UserRole } from "@prisma/client";
+
+declare module "next-auth" {
+  /**
+   * Returned by `auth`, `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
+   */
+  interface Session {
+    user: {
+      /** The user's postal address. */
+      role: UserRole;
+      /**
+       * By default, TypeScript merges new interface properties and overwrites existing ones.
+       * In this case, the default session user properties will be overwritten,
+       * with the new ones defined above. To keep the default session user properties,
+       * you need to add them back into the newly declared interface.
+       */
+    } & DefaultSession["user"];
+  }
+}
 
 export const { auth, handlers, signIn, signOut } = NextAuth({
+  callbacks: {
+    async session({ session, token }) {
+      if (session.user && token.sub) {
+        session.user.id = token.sub;
+      }
+      if (session.user && token.role) {
+        session.user.role = token.role as UserRole;
+      }
+      console.log(session);
+      return session;
+    },
+    async jwt({ token }) {
+      // If I don't have token.sub, it means that the user is not logged in
+      if (!token.sub) return token;
+      // We use getUserById because it is a much less expensive query than using getUserByEmail (because it is a primary key)
+      const existingUser = await getUserById(token.sub);
+      if (!existingUser) return token;
+      // We add the role to the token
+      token.role = existingUser.role;
+
+      return token;
+    },
+  },
   adapter: PrismaAdapter(db),
   session: { strategy: "jwt" },
   ...authConfig,
