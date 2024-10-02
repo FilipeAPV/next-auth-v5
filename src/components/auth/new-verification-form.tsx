60cbce47a95f01383019aa97ee8c6ca19abf1af7
Filipe Vieira <filipe.vieira@scott-sports.com>
2024-10-02 09:01:51 +0200

    email verification 44752

---
+++ src/components/auth/new-verification-form.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import CardWrapper from "./card-wrapper";
+import { BeatLoader } from "react-spinners";
+import { useSearchParams } from "next/navigation";
+import { useCallback, useEffect, useState } from "react";
+import { newVerification } from "@/actions/new-verification";
+import FormSuccess from "../form-success";
+import FormError from "../form-error";
+
+export const NewVerificationForm = () => {
+  const [error, setError] = useState<string | undefined>();
+  const [success, setSuccess] = useState<string | undefined>();
+  const searchParams = useSearchParams();
+  const token = searchParams.get("token");
+
+  const onSubmit = useCallback(() => {
+    if (!token) {
+      setError("Token not found");
+      return;
+    }
+    newVerification(token)
+      .then((data) => {
+        setSuccess(data.success);
+        setError(data.error);
+      })
+      .catch((error) => {
+        setError(error.message);
+      });
+  }, [token]);
+
+  useEffect(() => {
+    onSubmit();
+  }, [onSubmit]);
+
+  return (
+    <CardWrapper
+      headerLabel="Confirming your verification"
+      backButtonLabel="Back to login"
+      backButtonHref="/auth/login"
+    >
+      <div className="flex items-center justify-center w-full">
+        {!success && !error && <BeatLoader />}
+        <FormSuccess message={success}></FormSuccess>
+        <FormError message={error}></FormError>
+      </div>
+    </CardWrapper>
+  );
+};
